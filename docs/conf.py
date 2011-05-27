8c33e48225a8bb65b2f286a43bf6595bb0b9d378
Steve Losh <steve@stevelosh.com>
2011-05-27 14:31:01 -0400

    Increment version.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Steve Losh and Dumbwaiter Design'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.0'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
